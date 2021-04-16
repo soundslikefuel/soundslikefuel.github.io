b4063d50c975e1d032c7d466eb8467ad3a9669a6
Josh <josh@vaultinnovation.com>
2021-04-15 19:16:05 -0500

    Adding php folder and file

---
+++ php/meetme.php
@@ -0,0 +1,11 @@
+$user_agent = TODO;
+
+$command = $_POST['command'];
+
+$domain = $_POST['text'];
+
+$token = $_POST['HpZ0riVq9n6cjZVnX5LbUBQT'];
+
+$channel_id = $_POST['channel_id'];
+
+$slack_webhook_url = "https://hooks.slack.com/services/T0XXXXXXX/B0XXXXXXX/xxxxxxxxxxxxxxxxxxxxxxxx"; // replace that URL with your webhook URL
